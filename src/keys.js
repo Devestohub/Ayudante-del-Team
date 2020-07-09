0bee4238c4e82cf6e9d7333c7f19dbdacd91166f
HUGOVIDAFE <Hugo.vidal.ferre@gmail.com>
2020-07-09 14:52:42 +0200

    [09.07.2020] 0.1.5 - No beta update!

--- src/keys.js
+++ src/keys.js
@@ -4,6 +4,7 @@ module.exports = {
   discord: {
     owner: '324449297951096834',
     original: '492095012960600074',
+    beta: '730602411399577650',
     server: '378284847048818698',
     token: process.env.TOKEN
   }
