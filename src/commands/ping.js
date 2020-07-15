bd30f2e15239d8a8bd48a91a354a487e33af5c0c
devestoguy <devestoguy@gmail.com>
2020-07-15 08:19:15 +0200

    [15.07.20] Syncing!

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -15,7 +15,7 @@ module.exports = {
         .setTitle(':information_source: ' + API.langs.__('commands.ping.embed.title'))
         .setDescription(API.langs.__('commands.ping.embed.description'))
         .setTimestamp()
-        .setFooter("© " + new Date().getFullYear() + " " + client.user.username, message.client.user.displayAvatarURL())
+        .setFooter("© " + new Date().getFullYear() + " " + client.user.username, client.user.displayAvatarURL())
       );
       return setTimeout(function() {
         msg.edit(embed
@@ -23,12 +23,11 @@ module.exports = {
           .setTitle(':information_source: ' + API.langs.__('commands.ping.embedPong.title'))
           .setDescription(API.langs.__('commands.ping.embedPong.description', { latency: Math.floor(msg.createdTimestamp - msgc), API: Math.round(client.ws.ping) }))
           .setTimestamp()
-          .setFooter("© " + new Date().getFullYear() + " " + client.user.username, message.client.user.displayAvatarURL())
+          .setFooter("© " + new Date().getFullYear() + " " + client.user.username, client.user.displayAvatarURL())
         )
       }, 1500)
-      // return message.edit(`> <a:check:717805909350547568> **Pong! My latency is ${Math.floor(message.createdTimestamp - msgc)}ms. And the API Latency is ${Math.round(msg.client.ws.ping)}ms.**\n> <a:arrowright:717805890001961001> **_Requested by: ${msg.author}_**`);
     }
-    if (isNaN(args[0]) || args[0] > 3) {
+    if (isNaN(args[0]) || args[0] > 4) {
       return message.channel.send("You don't introduce a number, or the number is more of 4, so I don't go to execute the command :D")
     }
     for (let i = 0; i < args[0]; i++) {
@@ -37,17 +36,17 @@ module.exports = {
         .setTitle(':information_source: ' + API.langs.__('commands.ping.embed.title'))
         .setDescription(API.langs.__('commands.ping.embed.description'))
         .setTimestamp()
-        .setFooter("© " + new Date().getFullYear() + " " + client.user.username, message.client.user.displayAvatarURL())
-      ).then(message => {
+        .setFooter("© " + new Date().getFullYear() + " " + client.user.username, client.user.displayAvatarURL())
+      ).then(m => {
         setTimeout(function() {
-          message.edit(embed
+          m.edit(embed
             .setColor('#7289da')
             .setTitle(':information_source: ' + API.langs.__('commands.ping.embedPong.title'))
-            .setDescription(API.langs.__('commands.ping.embedPong.description', { latency: Math.floor(message.createdTimestamp - msgc), API: Math.round(client.ws.ping) }))
+            .setDescription(API.langs.__('commands.ping.embedPong.description', { latency: Math.floor(m.createdTimestamp - msgc), API: Math.round(client.ws.ping) }))
             .setTimestamp()
-            .setFooter("© " + new Date().getFullYear() + " " + client.user.username, message.client.user.displayAvatarURL())
+            .setFooter("© " + new Date().getFullYear() + " " + client.user.username, client.user.displayAvatarURL())
           )
-          msgc = message.createdTimestamp;
+          msgc = m.createdTimestamp;
         }, 1500)
       })
     }
